e915fe04e2f0b0e747648b3d37e4a1c00965f114
wajahatkhan96 <162484035+wajahatkhan96@users.noreply.github.com>
2025-04-29 20:41:52 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,205 @@
-# CS-598-Project-Wkhan25-and-XL170-
+# Unifying Heterogeneous Electronic Health Record Systems via Clinical Text-Based Code Embedding
+Kyunghoon Hur, Jiyoung Lee, Jungwoo Oh, Wesley Price, Young-Hak Kim, Edward Choi
+
+This repository provides official Pytorch code to implement DescEmb, a code-agnostic EHR predictive model.
+
+The paper can be found in this link:
+[Unifying Heterogeneous Electronic Health Record Systems via Clinical Text-Based Code Embedding](https://arxiv.org/abs/2108.03625)
+
+# Requirements
+
+* [PyTorch](http://pytorch.org/) version >= 1.8.1
+* Python version >= 3.7
+
+# Getting started
+## Prepare training data
+First, download the dataset from these links: 
+
+[MIMIC-III](https://physionet.org/content/iii/1.4/)
+
+[eICU](https://physionet.org/content/eicu-crd/2.0/)
+
+[ccs_multi_dx_tool_2015](https://www.hcup-us.ahrq.gov/toolssoftware/ccs/Multi_Level_CCS_2015.zip)
+
+[icd10cmtoicd9gem](https://data.nber.org/gem/icd10cmtoicd9gem.csv)
+
+Second, make directory sturcture like below:
+```
+data_input_path
+├─ mimic
+│  ├─ ADMISSIONS.csv
+│  ├─ PATIENTS.csv
+│  ├─ ICUSYAYS.csv
+│  ├─ LABEVENTES.csv
+│  ├─ PRESCRIPTIONS.csv
+│  ├─ PROCEDURES.csv
+│  ├─ INPUTEVENTS_CV.csv
+│  ├─ INPUTEVENTS_MV.csv
+│  ├─ D_ITEMDS.csv
+│  ├─ D_ICD_PROCEDURES.csv
+│  └─ D_LABITEMBS.csv
+├─ eicu
+│  ├─ diagnosis.csv
+│  ├─ infusionDrug.csv
+│  ├─ lab.csv
+│  ├─ medication.csv
+│  └─ patient.csv
+├─ ccs_multi_dx_tool_2015.csv
+└─ icd10cmtoicd9gem.csv
+
+```
+```
+data_output_path
+├─mimic
+├─eicu
+├─pooled
+├─label
+└─fold
+```
+Then run preprocessing code
+```shell script
+$ python preprocess_main.py
+    --src_data $data
+    --dataset_path $data_src_directory
+    --dest_path $run_ready_directory 
+```
+Note that pre-processing takes about 1hours in 128 cores of AMD EPYC 7502 32-Core Processor, and requires 60GB of RAM.
+
+# Examples
+## Pre-training a model
+### Pre-train a DescEmb model with Masked Language Modeling (MLM)
+
+```shell script
+$ python main.py \
+    --distributed_world_size $WORLDSIZE \
+    --input_path /path/to/data \
+    --src_data $data \
+    --task mlm \
+    --mlm_prob $percent \
+    --model $model
+```
+
+### Pre-train a CodeEmb model with Word2Vec
+
+```shell script
+$ python main.py \
+    --distributed_world_size $WORLDSIZE \
+    --input_path /path/to/data \
+    --src_data $data \
+    --task w2v
+    --model codeemb
+```
+$data should be set to 'mimic' or 'eicu'
+
+$percent should be set to probability (default: 0.3) of masking for MLM
+
+$model should be set to 'descemb_bert' or 'descemb_rnn'
+
+## Training a new model
+Other configurations will set to be default, which were used in the DescEmb paper.
+
+`$descemb` should be 'descemb_bert' or 'descemb_rnn'
+
+`$ratio` should be set to one of [10, 30, 50, 70, 100] (default: 100)
+
+`$value` should be set to one of ['NV', 'VA', 'DSVA', 'DSVA_DPE', 'VC']
+
+`$task` should be set to one of ['readmission', 'mortality', 'los_3day', 'los_7day', 'diagnosis']
+
+Note that `--input-path ` should be the root directory containing preprocessed data.
+### Train a new CodeEmb model:
+
+```shell script
+$ python main.py \
+    --distributed_world_size $WORLDSIZE \
+    --input_path /path/to/data \
+    --model ehr_model \
+    --embed_model codeemb \
+    --pred_model rnn \
+    --src_data $data \
+    --ratio $ratio \
+    --value_mode $value \
+    --task $task
+```
+### Train a new DescEmb model:
+
+```shell script
+$ python main.py \
+    --disrtibuted_world_size $WORLDSIZE \
+    --input_path /path/to/data \
+    --model ehr_model \
+    --embed_model $descemb \
+    --pred_model rnn \
+    --src_data $data \
+    --ratio $ratio \
+    --value_mode $value \
+    --task $task
+```
+Note: if you want to train with pre-trained BERT model, add command line parameters `--init_bert_params` or `--init_bert_params_with_freeze`. `--init_bert_params_with_freeze` enables the model to load and freeze BERT parameters.
+
+## Fine-tune a pre-trained model
+
+### Fine-tune a pre-trained CodeEmb model:
+
+```shell script
+$ python main.py \
+    --distributed_world_size $WORLDSIZE \
+    --input_path /path/to/data \
+    --model_path /path/to/model.pt \
+    --load_pretrained_weights \
+    --model ehr_model \
+    --embed_model codeemb \
+    --pred_model rnn \
+    --src_data $data \
+    --ratio $ratio \
+    --value_mode $value \
+    --task $task
+```
+### Fine-tune a pre-trained DescEmb model:
+```shell script
+$ python main.py \
+    --distributed_world_size $WORLDSIZE \
+    --input_path /path/to/data \
+    --model_path /path/to/model.pt \
+    --load_pretrained_weights \
+    --model ehr_model \
+    --embed_model $descemb \
+    --pred_model rnn \
+    --src_data $data \
+    --ratio $ratio \
+    --value_mode $value \
+    --task $task
+```
+
+## Transfer a trained model
+```shell script
+$ python main.py \
+    --distributed_world_size $WORLDSIZE \
+    --input_path /path/to/data \
+    --model_path /path/to/model.pt \
+    --transfer \
+    --model ehr_model \
+    --embed_model $embed_model \
+    --pred_model rnn \
+    --src_data $data \
+    --ratio $ratio \
+    --value_mode $value \
+    --task $task \
+```
+Note that `--embed_model` and `pred_model` should be matched with the transferred model.
+
+# License
+This repository is MIT-lincensed.
+
+# Citation
+Please cite as:
+```
+@misc{hur2021unifying,
+      title={Unifying Heterogenous Electronic Health Records Systems via Text-Based Code Embedding}, 
+      author={Kyunghoon Hur and Jiyoung Lee and Jungwoo Oh and Wesley Price and Young-Hak Kim and Edward Choi},
+      year={2021},
+      eprint={2108.03625},
+      archivePrefix={arXiv},
+      primaryClass={cs.LG}
+}
+```
---
+++ main.py
@@ -0,0 +1,194 @@
+import argparse
+import logging
+import logging.config
+import random
+import os
+import sys
+
+# should setup root logger before importing any relevant libraries.
+logging.basicConfig(
+    format="%(asctime)s | %(levelname)s %(name)s %(message)s)))",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level = os.environ.get("LOGLEVEL", "INFO").upper(),
+    stream = sys.stdout
+)
+logger = logging.getLogger(__name__)
+
+import numpy as np
+
+import torch
+import torch.multiprocessing as mp
+
+from trainers import Trainer, Word2VecTrainer
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--distributed_world_size', type=int, default=1
+    )
+
+    # checkpoint configs
+    parser.add_argument('--input_path', type=str, required=True)
+    parser.add_argument('--model_path', type=str)
+    parser.add_argument('--save_dir', type=str, default='checkpoints')
+    parser.add_argument('--save_prefix', type=str, default='checkpoint')
+
+    parser.add_argument(
+        '--patience', type=int, default=5,
+        help= (
+            'early stop training if valid performance does not '
+            + 'improve for N consecutive validation runs'
+        )
+    )
+    parser.add_argument(
+        '--disable_validation', action='store_true',
+        help='disable validation'
+    )
+
+    # dataset
+    parser.add_argument('--src_data', choices=['mimiciii', 'eicu', 'pooled'], type=str, required=True)
+    parser.add_argument('--eval_data', choices=['mimiciii', 'eicu', 'pooled'], type=str, default=None, required=False)
+    parser.add_argument('--value_mode', choices=['NV', 'VA','DSVA','DSVA_DPE','VC'], default='NV')
+    parser.add_argument('--fold', type=str, default=None)
+    parser.add_argument('--valid_subsets', type=str, default="valid, test")
+
+    parser.add_argument(
+        '--task',
+        choices=['readmission', 'mortality', 'los_3day', 'los_7day', 'diagnosis', 'mlm', 'w2v'],
+        type=str,
+        default='readmission',
+        help=""
+    )
+
+    # trainer
+    parser.add_argument('--seed', type=int, default=1)
+    parser.add_argument('--ratio', choices=['10', '30', '50', '70', '90', '100'], type=str, default= '100')
+    parser.add_argument('--n_epochs', type=int, default=1000)
+    parser.add_argument('--lr', type=float, default=1e-4)
+    parser.add_argument('--batch_size', type=int, default=128)
+    
+    # encoder model configs
+    parser.add_argument('--enc_embed_dim', type=int, default=128)
+    parser.add_argument('--enc_hidden_dim', type=int, default=256)
+
+    # predictive model configs
+    parser.add_argument('--rnn_layer', type=int, default=1)
+    parser.add_argument('--dropout', type=int, default=0.3)
+    parser.add_argument('--pred_embed_dim', type=int, default=128)
+    parser.add_argument('--pred_hidden_dim', type=int, default=256)
+    parser.add_argument('--max_event_len', type=int, default=150)
+    # mlm pretrain & finetune
+    parser.add_argument('--mlm_prob', type=float, default=0.3)
+    parser.add_argument('--load_pretrained_weights', action='store_true')
+
+    
+    # for transfer
+    parser.add_argument("--transfer", action="store_true")
+
+    # model
+    parser.add_argument(
+        '--model', type=str, required=True,
+        help='name of the model to be trained'
+    )
+    parser.add_argument(
+        '--embed_model', type=str, required=False,
+        help='name of the encoder model in the --model, '
+            'only used when --model has encoder-predictor structure'
+    )
+    parser.add_argument(
+        '--pred_model', type=str, required=False,
+        help='name of the predictor model in the --model, '
+            'only used when --model has encoder-predictor structure'
+    )
+
+    parser.add_argument('--bert_model', choices=['bert', 'bert_tiny', 'bert_mini'], type=str, default='bert_tiny')
+    parser.add_argument('--init_bert_params', action='store_true')
+    parser.add_argument('--init_bert_params_with_freeze', action='store_true')
+
+    return parser
+
+def main():
+    args = get_parser().parse_args()
+    args.valid_subsets = (
+        args.valid_subsets.replace(' ','').split(',')
+        if (
+            not args.disable_validation
+            and args.task not in ['mlm', 'w2v']
+            and args.valid_subsets
+        )
+        else []
+    )
+    args.device_ids = list(range(args.distributed_world_size))
+    set_struct(vars(args))
+
+    mp.set_sharing_strategy('file_system')
+    random.seed(args.seed)
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
+    torch.cuda.manual_seed_all(args.seed)  # if use multi-GPU
+    torch.backends.cudnn.deterministic = True
+
+    if args.task != 'w2v':
+        trainer = Trainer(args)
+    else:
+        trainer = Word2VecTrainer(args)
+    trainer.train()
+    logger.info("done training")
+
+
+def set_struct(cfg: dict):
+    root = os.path.abspath(
+        os.path.dirname(__file__)
+    )
+    from datetime import datetime
+    now = datetime.now()
+    from pytz import timezone
+    # apply timezone manually
+    now = now.astimezone(timezone('Asia/Seoul'))
+
+    output_dir = os.path.join(
+        root,
+        "outputs",
+        now.strftime("%Y-%m-%d"),
+        now.strftime("%H-%M-%S")
+    )
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    os.chdir(output_dir)
+
+    job_logging_cfg = {
+        'version': 1,
+        'formatters': {
+            'simple': {
+                'format': '[%(asctime)s][%(name)s][%(levelname)s] - %(message)s'
+            }
+        },
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler', 'formatter': 'simple', 'stream': 'ext://sys.stdout'
+            },
+            'file': {
+                'class': 'logging.FileHandler', 'formatter': 'simple', 'filename': 'train.log'
+            }
+        },
+        'root': {
+            'level': 'INFO', 'handlers': ['console', 'file']
+            },
+        'disable_existing_loggers': False
+    }
+    logging.config.dictConfig(job_logging_cfg)
+
+    cfg_dir = ".config"
+    os.mkdir(cfg_dir)
+    os.mkdir(cfg['save_dir'])
+
+    with open(os.path.join(cfg_dir, "config.yaml"), "w") as f:
+        for k, v in cfg.items():
+            print("{}: {}".format(k, v), file=f)
+
+
+if __name__ == '__main__':
+    main()
